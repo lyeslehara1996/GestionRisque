2e17048e3e788eb7f677b4336d5d3a34b98db564
lyeslehara1996 <lyeslehara88@gmail.com>
2022-10-06 16:47:24 +0200

    update routing

--- AuthApp/src/app/_services/UserService/user.service.ts
+++ AuthApp/src/app/_services/UserService/user.service.ts
@@ -9,7 +9,7 @@ import { AppUser } from 'src/app/Models/AppUser';
 })
 export class UserService {
 
-  PATH_API='http://localhost:8085/';
+  PATH_API='http://localhost:8085/api/';
 
    requestHeader =new HttpHeaders(
     {"No-Auth":"true"}
@@ -20,23 +20,23 @@ export class UserService {
 //get users methode 
 
   public getUsers(url:any):Observable <any> {
-    return this.httpClient.get(this.PATH_API+"api/user");
+    return this.httpClient.get(this.PATH_API+"users");
   }
 
   //add Users methode
   public AddUsers( appUser:Object):Observable <any> {
-    return this.httpClient.get(this.PATH_API+"api/user",appUser);
+    return this.httpClient.get(this.PATH_API+"Adduser",appUser);
   }
 
 
   //get roles methode 
   public getRoles(url:any):Observable <any> {
-    return this.httpClient.get(this.PATH_API+"api/user");
+    return this.httpClient.get(this.PATH_API+"Roles");
   }
 
   // add role methode 
   public AddRoles(url:any):Observable <any> {
-    return this.httpClient.get(this.PATH_API+"api/user");
+    return this.httpClient.get(this.PATH_API+"AddRole");
   }
 
   //add role to user methode 
--- AuthApp/src/app/_services/storageService/storage-s.service.ts
+++ AuthApp/src/app/_services/storageService/storage-s.service.ts
@@ -1,9 +1,40 @@
+import { Token } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { AppUser } from 'src/app/Models/AppUser';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageSService {
+  constructor() {}
 
-  constructor() { }
+  public signOut() {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(tocken: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, tocken);
+  }
+
+  public getJwt(): string | null {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user:any):void{
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public  getUSer():any{
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    return {};
+  }
 }
